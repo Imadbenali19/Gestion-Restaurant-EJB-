82562ad998cbc1db5b80d69d461c4576b6515ffa
Imad Ben-Ali <benali.ib19@gmail.com>
2023-01-06 19:47:18 +0000

    Delete FrontEnd directory

--- FrontEnd/src/utils/getPhotosOfRestau.js
+++
@@ -1,6 +0,0 @@
-export const getPhoto = async (id) => {
-    const result = await fetch(`http://localhost:8080/GestionRestauWS/restau/getPhotos/${id}/`);
-    const data = await result.json();
-    console.log(data) 
-    return data;
-}
