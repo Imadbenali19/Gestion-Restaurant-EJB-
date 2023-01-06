012142b1bba7b3e5307ba4ea397993e23532f4b7
“benali.ib19” <benali.ib19@gmail.com>
2023-01-06 19:49:54 +0000

    FrontEnd web app Zakaria

---
+++ FrontEnd/node_modules/@mantine/core/lib/Floating/types.d.ts
@@ -0,0 +1,5 @@
+export declare type FloatingPlacement = 'end' | 'start';
+export declare type FloatingSide = 'top' | 'right' | 'bottom' | 'left';
+export declare type FloatingPosition = FloatingSide | `${FloatingSide}-${FloatingPlacement}`;
+export declare type ArrowPosition = 'center' | 'side';
+//# sourceMappingURL=types.d.ts.map
